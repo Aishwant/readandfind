080111e225ecea41230209bd4bb8539785f529c1
Aishwant <aishwantg@gmail.com>
2018-04-11 18:35:28 -0500

    change in the design

--- forgotpwd.php
+++ forgotpwd.php
@@ -7,17 +7,23 @@
 <head>
     <title>Forgot Password</title>
     <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
 </head>
 <body>
 
-<?php form_setup_start("Verification") ?>
+<div class="left-img"></div>
 
-        Name: &nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="name" class="input-style" id="name"><br /><br />
+<section class="content">
+    <?php form_setup_start("Verification") ?>
+
+        <br /><br />
         Email: &nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="email" class="input-style" id="email"><br /><br />
-    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 
-<?php form_setup_end(); ?>
+        <br /><br /><br />
+        <button type="submit" name="submit" class="btn btnCSS">Submit</button>
 
+    <?php form_setup_end(); ?>
+</section>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="js/bootstrap.min.js"></script>
