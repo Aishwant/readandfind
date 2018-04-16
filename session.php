b38dd572eaee94d2b0e2ed97da2b69ab361a600a
Aishwant <aishwantg@gmail.com>
2018-04-16 16:11:55 -0500

    created session.php and functionality

---
+++ session.php
@@ -0,0 +1,27 @@
+<?php
+
+    session_start();
+
+    function message(){
+        if ($_SESSION['message'] != null){
+        }
+    }
+
+    function verify_login(){
+        if((!isset($_SESSION['email_id']) && $_SESSION['email_id'] === null) && (!isset($_SESSION['user_id']) && $_SESSION['user_id'] === null)){
+            $_SESSION['message'] = 'You must login first!';
+            header('Location: /');
+            exit;
+        }
+    }
+
+    function login_in(){
+        if((!isset($_SESSION['email_id']) && $_SESSION['email_id'] === null) && (!isset($_SESSION['user_id']) && $_SESSION['user_id'] === null)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+
+?>
