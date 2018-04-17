9d4ddacd0a46cf423099eb36d282cc7a0a865cf5
Aishwant <aishwantg@gmail.com>
2018-04-17 01:48:06 -0500

    list recent read books (template)

--- home.php
+++ home.php
@@ -16,4 +16,27 @@
     </div>
     <h4><center><?php echo"Hello ".$_SESSION['fname'];?></center></h4>
 
+    <div class='container'>
+        <div class="row">
+            <div class = "col-lg-6 col-md-5">
+            </div>
+            <div class = "col-lg-6 col-md-7 col-sm-12 col-xs-12">
+                <br />Recent Entries: <br /><br />
+                <div class="books">
+
+                    <center>Title:<br /></center><br />
+
+                    Author:<br />
+                    Genre:<br />
+                    Year Read:<br />
+
+                    <div style="text-align:right; padding-right:5%;">
+                        <a href="#">Remove</a> | <a href="#">Edit</a>
+                    </div>
+                    <br/>
+
+                </div>
+            </div>
+        </div>
+    </div>
    <?php footer(); ?>
