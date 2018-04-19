d5c85fa023b95660fb1ad0388637da3f10041814
Aishwant <aishwantg@gmail.com>
2018-04-18 21:37:23 -0500

    New page that can search people with common user-entered books

---
+++ commonbooks.php
@@ -0,0 +1,57 @@
+<?php
+
+include('included_functions.php');
+
+require_once('session.php');
+
+$mysqli=db_connection();
+
+verify_login();
+
+new_header();
+nav_bar();
+
+if (isset($_POST['submit'])){
+    if(isset($_POST['searchbook']) && !empty($_POST['searchbook'])){
+        $query="select Books.Book_Name, Books.Book_ID, Users.FName, Users.LName, Users_Books.Year_Read ";
+        $query.="from Books natural join Users_Books natural join Users";
+        $result = $mysqli->query($query);
+        $code=null;
+        $val=null;
+        while($row = $result->fetch()){
+            if($row['Book_Name']==$_POST['searchbook']){
+                $code = $code.$row['FName']." ".$row['LName']."<br />";
+            }
+        }
+        if($code ===null){ $code = 1;}
+    }else{
+        $val = 'Nothing to Display';
+    }
+}
+
+?>
+<div class="left-img"></div>
+<section class="content">
+    <div class="container">
+        <form method="POST" action="">
+            <input type="text" name="searchbook" placeholder="Name the book">
+            <input type="submit" name="submit" value="search" class="btn btnCSS border">
+        </form>
+        <div class="col-lg-12" id="updatedfield">
+            <?php
+                if($code!=1 && $code !==null){
+                    $val = $code;
+                }elseif($code){
+                    $val = "No Match";
+                }
+                echo "<script>document.getElementById('updatedfield').innerHTML='$val'</script>";
+            ?>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<?php footer(); ?>
