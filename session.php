04c4ed01f9c3db5f1e161e556f6293dbc3394f82
Aishwant <aishwantg@gmail.com>
2018-04-24 19:25:16 -0500

    message function message = ""

--- session.php
+++ session.php
@@ -20,6 +20,7 @@
 
     function logged_in(){
         if((!isset($_SESSION['email_id']) && $_SESSION['email_id'] === null) && (!isset($_SESSION['user_id']) && $_SESSION['user_id'] === null)){
+            $_SESSION['message'] = 'You must login first!';
             return false;
         }else{
             return true;
